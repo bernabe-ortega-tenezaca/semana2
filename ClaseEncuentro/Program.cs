3856e0fbff844cbf5b106e8218583f89de451391
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2026-06-06 11:41:14 -0500

    inicio semana2

---
+++ ClaseEncuentro/Program.cs
@@ -0,0 +1,16 @@
+﻿// Clase encuentro 2
+// Operaciones con datos primitivos
+// Bernabé Ortega
+Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
+
+string nombre = "Bernabé Ortega"; // declaramos una variable tipo cadena
+int edad = 25; // variable tipo entero
+double altura = 1.70; // variable tipo decimal
+
+Console.WriteLine(nombre);
+Console.WriteLine(edad);
+Console.WriteLine(altura);
+
+// Concatenar cadenas
+Console.WriteLine("hola" + " " + nombre);
+Console.WriteLine("hola " + nombre);
